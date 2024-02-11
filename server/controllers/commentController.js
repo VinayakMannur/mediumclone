8b5626508fbaa76ae1e40e0fc7c34e69223b9ef3
VinayakMannur <Vinayakmannur20@gmail.com>
2024-02-11 19:30:06 +0530

    api for fetching comments

---
+++ server/controllers/commentController.js
@@ -0,0 +1,16 @@
+const Comment = require('../models/comment')
+
+exports.fetchComments = async (req, res) =>{
+    try {
+        const id = req.params.id;
+        console.log(id);
+
+        const comments = await Comment.find({postId: id}).populate('userId', '-googleId -email').exec()
+        return res.status(200).send({comments, Message: true})
+
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Internal Server Error')
+    }
+}
